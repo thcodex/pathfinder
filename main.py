76e423a2a6124d58ca7fd7d30b3d56d1611c08cd
Thomas Gil <thomasgil913@gmail.com>
2021-12-19 13:34:54 -0500

    Updated main

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import pygame
 
 if __name__ == "__main__":
     pygame.init()
-    win = pygame.display.set_mode((800, 800))
-    game_loop(win, 800)
+    WIDTH = 800
+    win = pygame.display.set_mode((WIDTH, WIDTH))
+    game_loop(win, WIDTH)
